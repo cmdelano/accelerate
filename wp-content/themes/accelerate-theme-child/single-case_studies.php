3b6d4cbb01152c8ad3013647b0609c1519c1d9f1
cmdelano <cmdelano@gmail.com>
2016-02-14 18:46:30 -0500

    added advanced custom fields (ACF) plugin

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -12,8 +12,39 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>				
-				<?php the_content(); ?>
+			
+			<?php while ( have_posts() ) : the_post();
+	
+			$size = "full";
+			$services = get_field('services');
+			$client = get_field('client');
+			$site = get_field('site');
+			$image_1 = get_field('image_1');
+			$image_2 = get_field('image_2');
+			$image_3 = get_field('image_3'); ?>
+
+			<aside class="case-study-sidebar">
+				<h2><?php the_title(); ?></h2>
+				<h5><?php echo $services; ?></h5>
+				<h3><?php echo $client; ?></h3>
+					<?php the_content(); ?>
+				<h4><a href="<?php echo $site; ?>">Visit Live Site</a></h4>
+			</aside>
+
+			<div class="case-study-images">
+				<?php if ($image_1) { ?>
+					<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+				<?php } ?>
+
+				<?php if ($image_2) { ?>
+					<?php echo wp_get_attachment_image( $image_2, $size ); ?>
+				<?php } ?>
+
+				<?php if ($image_3) { ?>
+					<?php echo wp_get_attachment_image( $image_3, $size ); ?>
+				<?php } ?>	
+			</div>	
+				
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
