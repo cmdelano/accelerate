7093a360e0f706ea0bc7fc8138cd25bb108f784e
cmdelano <cmdelano@gmail.com>
2016-02-21 01:02:02 -0500

    added .narrow-conact to css and page.php for contact form

--- wp-content/themes/accelerate-theme-child/page.php
+++ wp-content/themes/accelerate-theme-child/page.php
@@ -16,11 +16,13 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
+			<div class="<?php if (is_page('contact_form')) { echo 'narrow-contact'; } ?>";
+			</div><!-- .narrow-contact -->
 			<?php while ( have_posts() ) : the_post(); ?>
 				<h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
-
+			
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
